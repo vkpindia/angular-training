478ef5bac545bb81806a03bbf42f73474001b72a
vkpindia <infovkpg@gmail.com>
2020-10-14 09:23:43 +0530

    keyvalue pipe

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { users } from './user';
 import { StudentT } from '../models/student.model';
+import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'app-hero-details',
@@ -11,13 +12,34 @@ export class HeroDetailsComponent implements OnInit {
 
   public users: Array<StudentT>;
   public tableHeader: Array<string>;
+  public userRecord: any;
+
+  originalOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+    return 0;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    /* this.userRecord = {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org'
+    };
+    [['id', '1'], ['name', 'Lean Graham']]
+    console.log(' this.userRecord', this.userRecord);
+
+    for (const userKey in this.userRecord) {
+      console.log('key', userKey);
+      console.log('value', this.userRecord[userKey]);
+    } */
 
     this.users = users;
     this.tableHeader = [
+      'ID',
       'name',
       'username',
       'email',
